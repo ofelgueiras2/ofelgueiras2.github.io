eaa31f93c57a33e413350540d36fac76a48a9df6
ofelgueiras2 <ofelgueiras2@gmail.com>
2025-03-22 09:24:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -31,9 +31,9 @@ const tabelas = {
     LuzigazFee: { inicio: "Z27", fim: "Z36" },
     TARPotencias: { inicio: "Z6", fim: "Z15" },
     detalheTarifarios: { inicio: "AM5", fim: "AM25"},
-    tarifariosExtra: { inicio: "C68", fim: "C77"},
-    detalheTarifariosExtra: { inicio: "B68", fim: "B77"},
-    preçosSimplesExtra: { inicio: "D68", fim: "W77"},
+    tarifariosExtra: { inicio: "C68", fim: "C79"},
+    detalheTarifariosExtra: { inicio: "B68", fim: "B79"},
+    preçosSimplesExtra: { inicio: "D68", fim: "W79"},
 };
 
 const variaveis = {
