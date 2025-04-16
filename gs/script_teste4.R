a9c218bcdc2ac70afbfee6d9beb504b905821897
ofelgueiras2 <ofelgueiras2@gmail.com>
2025-04-16 10:03:59 +0100

    Update script_teste4.R

--- gs/script_teste4.R
+++ gs/script_teste4.R
@@ -155,7 +155,7 @@ dados_parquet <- bind_rows(df_old, dados_csv2)
 # (Opcional) Gravar ficheiro combinado
 write_parquet(
   dados_parquet,
-  "Precos_20100101_hoje.parquet",
+  "gs/Precos_20100101_hoje.parquet",
   compression = "snappy",
   use_dictionary = TRUE
 )
