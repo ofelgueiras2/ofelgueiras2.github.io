d05a9c5d5b4c61baab9dd3aba17b1028e669e39a
ofelgueiras2 <ofelgueiras2@gmail.com>
2025-07-05 08:16:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1449,7 +1449,7 @@ if (!isNaN(omieParseado)) {
             // Apenas para “EDP indexado” criamos a tooltipText e a classe
             let cellAttrs = ' class="internop"';
             if ((tarifa.nome === "EDP indexado" || tarifa.nome.startsWith("EDP: Eletricidade Indexada")) && incluirEDP && potenciaNum >=3.45) {
-                const descontoMsg = "Valor apresentado inclui desconto mensal de 10€ válido nos primeiros 10 meses, para adesões até 30/6/2025";
+                const descontoMsg = "Valor apresentado inclui desconto mensal de 10€ válido nos primeiros 10 meses, para adesões até 30/9/2025";
                 const tooltipText = descontoMsg;
                 cellAttrs = ` class="internop has-tooltip mais-indicator" data-tippy-content="${tooltipText}"`;        
             }
